7f46470b17104e703d2699a4bbd6ca089d14afa2
Michael Yu <michael.yu2016@gmail.com>
2018-05-03 17:53:06 -0700

    made card generic

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 const Card = props => (
   <div className="card">
-    <img src="https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg" alt="A Day in LA" />
+    {props.img && props.alt && (
+      <img src={props.img} alt={props.alt} />
+    )}
     <div className="body">
-      <h1>{props.project.title}</h1>
-      <p>{props.project.text}</p>
+      {props.title && (
+        <h1>{props.title}</h1>
+      )}
+      {props.text && (
+        <p>{props.text}</p>
+      )}
     </div>
   </div>
 );
