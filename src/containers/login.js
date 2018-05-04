ee8fa9e17447ac3b120001aeb629ac718ef17f45
Bryan Wong <bryanw0ng@ucla.edu>
2018-05-04 14:19:37 -0700

    Move login api calls to own file

--- src/containers/login.js
+++ src/containers/login.js
@@ -4,70 +4,8 @@ import { push } from 'react-router-redux';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
+import { login, createAccount } from '../api/api.js';
 import Button from '../components/Button';
-
-const createAccount = async (data) => { //bs fetch function to test stuff... change later
-  try{
-    const resp = await fetch('http://localhost:3000/admins/new/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
-    });
-    const status = resp.status;
-    const json = await resp.json();
-    
-    if(status !== 200){
-    	if(json.err){
-    		throw new Error(json.err);
-    	} else{
-    		throw new Error('Failed to create account, no error code provided.');
-    	}
-    }
-
-    return{
-    	err: false,
-    	json: json,
-    	confirm: 'Account successfully created.',
-    }
-
-  } catch(e){
-    return {
-      err: e.message
-    }
-  }
-}
-
-const login = async (data) => {
-  try{
-    const resp = await fetch('http://localhost:3000/admins/login/', {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
-    });
-    const status = resp.status;
-    const json = await resp.json();
-
-    if(status !== 200){
-    	if(json.err){
-    		throw new Error(json.err);
-    	}
-    	else{
-    		throw new Error('Failed to log in, no error code provided.');
-    	}
-    }
-
-    return{
-    	err: false,
-    	json: json,
-    	confirm: 'Login successful.',
-    }
-
-  } catch(e){
-    return{
-      err: e.message,
-    }
-  }
-}
 
 class Login extends React.Component {
 	constructor(props){
