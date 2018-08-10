c19f19657ab284695602f97ca1f76c3e026cd6e7
Michael Yu <michael.yu2016@gmail.com>
2018-08-10 00:05:36 -0700

    network layer + start of api layer
    network layer and beginning of api layer

---
+++ src/api/utils/request_api.js
@@ -0,0 +1,112 @@
+/**
+ * Request is a a custom Network Layer.  Its main benefits of using it over fetch include
+ * a longer chain of promises which allows us to write error handling code all in one place.
+ * Request will give json back or some standard, meaningful error so that the errors and parsing
+ * don't have to be handled higher up in the call stack.
+ */
+
+class Request {
+  static headers() {
+    return {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  static get(url) {
+    return this.fetchWrapper(url, null, 'GET')
+  }
+  static post(url, params) {
+    return this.fetchWrapper(url, params, 'POST')
+  }
+
+  static isJSON(response) {
+    // returns true if response is of type JSON
+    const contentType = response.headers.get('content-type');
+
+    return contentType && contentType.includes('application/json');
+  }
+
+  /**
+   * @param {Response} response
+   * @return {Promise} 
+   */
+  static parseResponseBody(response) {
+    if (this.isJSON(response)) {
+      return response.json();
+    }
+
+    return response.text();
+  }
+
+  /**
+   * @param {String} url
+   * @param {Object?} params
+   * @param {String} verb
+   * @return {Promise} 
+   */
+  static fetchWrapper(url, params, verb) {
+
+    // for logging/debugging
+    console.log(`${verb} ${url}`, params);
+
+    // baseURL is URL without params
+    // useful if settings/filters are in URL
+    const baseURL = url.split('?')[0];
+
+    // options are the complicated object of options that are passed in a fetch response
+    // another benefit of abstracting this portion away
+    let options = Object.assign({ method: verb }, params ? { body: JSON.stringify(params) } : null);
+    options.headers = Request.headers();
+
+    let fetchResponse, fetchStatusCode, errorResponse;
+
+    return fetch(url, options).then(response => {
+      fetchResponse = response;
+      fetchStatusCode = response.status;
+
+      // success, simply return promise of parsed response
+      if (fetchStatusCode === 200 || fetchStatusCode === 201) {
+        return Request.parseResponseBody(fetchResponse);
+      }
+      else {
+        // throw errors if bad status
+        return Request.parseResponseBody(response).then(body => {
+          errorResponse = body;
+          throw new Error(`${url} failed status=${response.status} errorResponse=${errorResponse}`);
+        })
+        .catch(() => {
+          // catch error if parseResponseBody throws an error
+          throw new Error(`${url} failed status=${response.status}`);
+        });
+      }
+    })
+    .catch(exception => {
+      // catch JSE
+
+      // give details on what the error was so it doesn't have to be deciphered up the call stack
+      let errorType;
+      if (!fetchResponse) {
+        // no response at all
+        errorType = 'NETWORK';
+      }
+      else if (fetchStatusCode === 200) {
+        errorType = 'PARSE RESPONSE';
+      } else {
+        // generic error
+        errorType = 'API CALL';
+      }
+
+      // a meaningful error
+      const errorDetail = {
+        errorType,
+        errorResponse,
+        exception: exception.message,
+        statusCode: fetchStatusCode
+      };
+
+      return Promise.reject(errorDetail);
+    });
+  }
+}
+
+export default Request;
--- src/containers/landing.js
+++ src/containers/landing.js
@@ -4,12 +4,36 @@ import { push } from 'react-router-redux';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
+import AdminClient from '../api/admin_client/index'
+
 import Button from '../components/Button';
 
 class Landing extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+
+    this.createAdmin = this.createAdmin.bind(this);
+  }
+
+  createAdmin() {
+    // for testing
+
+    const payload = {
+      firstName: "the last",
+      lastName: "boob",
+      email: "boob29@boob.com",
+      username: "blah70",
+      password: "password"
+    }
+
+    AdminClient.getSingleton().createAdmin(payload)
+      .then(response => {
+        console.log('createAdmin', response);        
+      })
+      .catch(error => {
+        console.log('createAdmin failed', error);
+      })
   }
 
   render() {
@@ -22,6 +46,8 @@ class Landing extends React.Component {
     return (
       <div className="display">
         <Button label="see projects" color="primary" onClick={onClickView} />
+
+        <Button label="create an admin" color="primary" onClick={this.createAdmin} />
       </div>
     );
   }
