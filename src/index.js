9995fae05cc78ed3f3e9ce6833a26e84cecf17f0
Bryan Wong <bryanw0ng@ucla.edu>
2018-08-20 19:38:39 -0700

    flow directives

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
-import { store, history } from './reducers/store';
+/**
+ * Add a description about this file here.
+ *
+ * @flow
+ *
+ */
 
-import App from './containers/app';
-import './styles/index.css';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "react-router-redux";
+import { store, history } from "./reducers/store";
 
-const target = document.querySelector('#root');
+import App from "./containers/app";
+import "./styles/index.css";
+
+const target = document.querySelector("#root");
 
 render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <div>
-        <App />
-      </div>
-    </ConnectedRouter>
-  </Provider>,
-  target
+	<Provider store={store}>
+		<ConnectedRouter history={history}>
+			<div>
+				<App />
+			</div>
+		</ConnectedRouter>
+	</Provider>,
+	target
 );
