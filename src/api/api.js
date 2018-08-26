ccdbc294f61d5a534177cff2d4ba509b6adb3c51
Bryan Wong <bryanw0ng@ucla.edu>
2018-08-26 15:24:39 -0700

    add flow

--- src/api/api.js
+++ src/api/api.js
@@ -5,99 +5,97 @@
  *
  */
 
-
-const createAccount = async (data) => { //bs fetch function to test stuff... change later
-  try{
-    const resp = await fetch('http://localhost:3000/admins/new/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
-    });
-    const status = resp.status;
-    const json = await resp.json();
-    
-    if(status !== 200){
-    	if(json.err){
-    		throw new Error(json.err);
-    	} else{
-    		throw new Error('Failed to create account, no error code provided.');
-    	}
-    }
-
-    return{
-    	err: false,
-    	json: json,
-    	confirm: 'Account successfully created.',
-    }
-
-  } catch(e){
-    return {
-      err: e.message
-    }
-  }
-}
-
-const login = async (data) => {
-  try{
-    const resp = await fetch('http://localhost:3000/admins/login/', {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
+const createAccount = async data => {
+  //bs fetch function to test stuff... change later
+  try {
+    const resp = await fetch("http://localhost:3000/admins/new/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
     });
     const status = resp.status;
     const json = await resp.json();
 
-    if(status !== 200){
-    	if(json.err){
-    		throw new Error(json.err);
-    	}
-    	else{
-    		throw new Error('Failed to log in, no error code provided.');
-    	}
-    }
-
-    return{
-    	err: false,
-    	json: json,
-    	confirm: 'Login successful.',
-    }
-
-  } catch(e){
-    return{
-      err: e.message,
-    }
-  }
-}
-
-const createApp = async (data) => {
-  try{
-    const resp = await fetch('http://localhost:3000/apps/new', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
-    });
-    const status = resp.status;
-    const json = await resp.json();
-
-    if(status !== 200){
-      if(json.err){
+    if (status !== 200) {
+      if (json.err) {
         throw new Error(json.err);
-      }
-      else{
-        throw new Error('Failed to submit application, no error code provided.');
+      } else {
+        throw new Error("Failed to create account, no error code provided.");
       }
     }
 
-    return{
+    return {
       err: false,
       json: json,
-      confirm: 'App creation successful.',
-    }
-  } catch(e){
-    return{
+      confirm: "Account successfully created."
+    };
+  } catch (e) {
+    return {
       err: e.message
-    }
+    };
   }
-}
+};
 
-export { login, createAccount, createApp }
+const login = async data => {
+  try {
+    const resp = await fetch("http://localhost:3000/admins/login/", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+    const status = resp.status;
+    const json = await resp.json();
+
+    if (status !== 200) {
+      if (json.err) {
+        throw new Error(json.err);
+      } else {
+        throw new Error("Failed to log in, no error code provided.");
+      }
+    }
+
+    return {
+      err: false,
+      json: json,
+      confirm: "Login successful."
+    };
+  } catch (e) {
+    return {
+      err: e.message
+    };
+  }
+};
+
+const createApp = async data => {
+  try {
+    const resp = await fetch("http://localhost:3000/apps/new", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+    const status = resp.status;
+    const json = await resp.json();
+
+    if (status !== 200) {
+      if (json.err) {
+        throw new Error(json.err);
+      } else {
+        throw new Error(
+          "Failed to submit application, no error code provided."
+        );
+      }
+    }
+
+    return {
+      err: false,
+      json: json,
+      confirm: "App creation successful."
+    };
+  } catch (e) {
+    return {
+      err: e.message
+    };
+  }
+};
+
+export { login, createAccount, createApp };
