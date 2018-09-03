5ea1279ee952ec9460abcc41b64cf7ebe165cff7
Steven La <stevenla@g.ucla.edu>
2018-09-03 00:01:18 -0700

    removed some testing variables

--- src/containers/about.js
+++ src/containers/about.js
@@ -16,12 +16,11 @@ class About extends React.Component {
       {'id': 'first_name', 'display': 'First Name'},
       {'id': 'last_name', 'display': 'Last Name'},
       {'id': 'year', 'display': 'Year'},
-      {'id': 'resume', 'display': 'Resume', 'link': true},
-      {'id': 'response', 'display': 'Response'}
+      {'id': 'resume', 'display': 'Resume', 'link': true}
     ];
 
     const dataArr = [
-      {'first_name': 'Steven', 'last_name': 'La', 'year': 3, 'resume': 'https://drive.google.com/file/d/13pPVe7p-mRo0yD1YaFMt6y4mgb9BaRNu/view', 'response': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ultricies turpis erat, sit amet convallis augue aliquam ac. Integer vel tristique mi, eu mollis nisi. Quisque a dui sit amet turpis commodo interdum. Aliquam placerat congue nisi id bibendum. Nulla bibendum ex non dolor rutrum placerat. Fusce lobortis tempus neque, nec tincidunt leo. Aliquam et nisl luctus, accumsan quam at, maximus lectus. Proin ornare a diam fringilla elementum. Cras venenatis massa tortor, vel luctus leo condimentum eu. Phasellus sit amet leo ut eros tristique venenatis. Nulla facilisi. Integer eu ante congue libero posuere tincidunt.'},
+      {'first_name': 'Steven', 'last_name': 'La', 'year': 3, 'resume': 'https://github.com/'},
       {'first_name': 'Michael', 'last_name': 'Yu', 'year': 3, 'resume': 'https://www.google.com'},
       {'first_name': 'Alex', 'last_name': 'Xu', 'year': 2, 'resume': 'https://www.facebook.com'},
       {'first_name': 'Bryan', 'last_name': 'Wong', 'year': 3, 'resume': 'https://www.twitter.com'}
