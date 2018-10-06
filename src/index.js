c681bcfaa2c7a5041d5be54e5429583f6f70f4ae
Michael Yu <michael.yu2016@gmail.com>
2018-10-05 17:08:43 -0700

    deployment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "react-router-redux";
+import "babel-polyfill";
 import { store, history } from "./reducers/store";
 
 import App from "./containers/app";
