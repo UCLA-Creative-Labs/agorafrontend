1b6c124486b860e7c4d2bfadf61078bc7ff85a5a
Bryan Wong <bryanw0ng@ucla.edu>
2018-10-09 21:26:56 -0700

    open up apps for fucking cody swain

--- src/containers/apply.js
+++ src/containers/apply.js
@@ -5,7 +5,6 @@ import { createApplication } from "../actions/applications";
 import Button from "../components/Button";
 import ApplyProjectMember from "../components/ApplyProjectMember";
 import SuccessScreen from "../components/SuccessScreen";
-
 import puzzle1 from "../assets/images/puzzle1.png";
 
 class Apply extends React.Component {
@@ -14,8 +13,8 @@ class Apply extends React.Component {
       <div className="apply-content">
         <img className="apply-puzzle" src={puzzle1} />
         <div className="apply-main">
-          {/* <ApplyProjectMember /> */}
-          <SuccessScreen text="Applications are now closed. Thanks!" />
+          <ApplyProjectMember />
+          {/* <SuccessScreen text="Applications are now closed. Thanks!" /> */}
         </div>
       </div>
     );
